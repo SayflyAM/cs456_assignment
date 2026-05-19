8d8faf3365b478793f2f4bad8a6d49b96a153734
Sayf Ammar <say.issa@uot.edu.ly>
2026-05-19 12:30:25 +0200

    work on assignmnet implemnt idea of fitness function for gentatic we will use later.

---
+++ assignment_3.cpp
@@ -0,0 +1,19 @@
+
+
+int calculateFitness(const vector<int>& board) {
+	int fitness = 0;
+	int n= board.size();
+
+    // this for outer loop for queen compersions
+	for (int i=0 ; i<n; i++){
+		// compersions after queen one
+		for (int j= i+1; j<n; j++) {
+			if() {
+				fitness++;
+			}
+		}
+	}
+    return fitness;
+
+
+} 
