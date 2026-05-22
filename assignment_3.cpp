490fc1e5a377c940fb81765f5e198cc5aad6a2f3
Sayf Ammar <say.issa@uot.edu.ly>
2026-05-22 23:03:32 +0200

    make important thing in gentaic alogo

--- assignment_3.cpp
+++ assignment_3.cpp
@@ -16,4 +16,76 @@ int calculateFitness(const vector<int>& board) {
     return fitness;
 
 
-} 
+}
+
+int selection(const vector<vector<int>>& poplution) {
+	int total_fitness =0;
+    int n = poplution.size();
+	for (int i=0;i<n;i++){
+		
+		total_fitness += calculateFitness(poplution[i]);
+
+	}
+    
+	int random_num = rand() % total_fitness;
+	int running_sum =0;
+
+	for (int i = 0; i<n ;i++) {
+		running_sum += calculateFitness(poplution[i]);
+
+		if (running_sum >= random_num){
+			return i;
+		}
+
+	}
+    return 0;
+
+}
+
+
+
+
+vector<int> crossover(const vector<int>& parent1, const vector<int>& parent2) 
+{
+	int n =  parent1.size();
+
+	vector<int> child(n);
+     
+	int c =  rand() % n;
+
+	for (int i=0; i < n; i++) {
+	
+		if(i < c){
+			child[i] = parent1[i];
+		}else{
+
+			child[i] = parent2[i];
+		}
+		}
+	return child;
+
+
+
+}
+
+
+
+void mutate(vector<int>& child) {
+	int n =  child.size();
+
+	int column = rand() % n;
+	int row =  rand() % n;
+
+	child[column] = row;
+}
+
+
+vector<int> intial_pop(){
+     vector<int> pop(8);
+	for(int i=0 ; i < 8 ; i++ ){
+		pop[i] = rand() % 8 ;
+	}
+
+	return pop;
+
+}
